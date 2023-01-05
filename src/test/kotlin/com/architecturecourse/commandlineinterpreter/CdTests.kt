6d998e7f0527582e73e14ebea4af55db271e03d8
Misha Diagilev <mishadyagilev@gmail.com>
2023-01-05 19:59:25 +0300

    valid cd tests

--- src/test/kotlin/com/architecturecourse/commandlineinterpreter/CdTests.kt
+++ src/test/kotlin/com/architecturecourse/commandlineinterpreter/CdTests.kt
@@ -8,38 +8,134 @@ import com.architecturecourse.commandlineinterpreter.entities.utils.CommandType
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import java.io.File
+import java.nio.file.Files
+import java.nio.file.FileSystems
 import java.util.*
+import com.architecturecourse.commandlineinterpreter.entities.command.CDCommand
+import kotlin.contracts.contract
 
 class CdTests {
 
+
+    //    main->TestForTests
     @Test
-    fun testExecuteCDValid() {
+    fun testExecuteCDValidNextTestDir() {
         var startedPath = System.getProperty("user.dir")
 
-
-        val args = listOf("..").map { Arg(it) }
+        val args = listOf("testForTests").map { Arg(it) }
         val cmdCD = CommandFactoryImpl().createCommand(CommandType.CD, args)
         cmdCD.execute(VariableContextImpl())
+
+        val expected = File(File(startedPath).absolutePath + File.separator + "testForTests").absolutePath
+
+        Assertions.assertEquals(expected, SystemStateSingletonImpl.instance.getPath())
+    }
+
+    //    main->notValidDir
+    @Test
+    fun testExecuteCDNotValidNextTestDir() {
+        var startedPath = System.getProperty("user.dir")
+
+        val args = listOf("notValid").map { Arg(it) }
+        val cmdCD = CommandFactoryImpl().createCommand(CommandType.CD, args)
+        cmdCD.execute(VariableContextImpl())
+
+//        val expected = File(startedPath).absolutePath
+        val expected = File(File(startedPath).absolutePath.split(File.separator)[0] + File.separator).absolutePath
+
+        Assertions.assertEquals(expected, SystemStateSingletonImpl.instance.getPath())
+    }
+
+    //    main->testForTests->main
+    @Test
+    fun testExecuteCDValidNextTestDirAndReturnBack() {
+        var startedPath = System.getProperty("user.dir")
+
+        val args = listOf("testForTests").map { Arg(it) }
+        val cmdCD = CommandFactoryImpl().createCommand(CommandType.CD, args)
+        cmdCD.execute(VariableContextImpl())
+
+        val argsSecond = listOf("..").map { Arg(it) }
+        val cmdCDSecond = CommandFactoryImpl().createCommand(CommandType.CD, argsSecond)
+        cmdCDSecond.execute(VariableContextImpl())
 
         val expected = File(startedPath).absolutePath
 
         Assertions.assertEquals(expected, SystemStateSingletonImpl.instance.getPath())
     }
+
+    //  main->testForTests->second->first->first
     @Test
-    fun testExecuteCDValidNextDir() {
+    fun testExecuteCDValidNextTestDirAbsolutePath() {
         var startedPath = System.getProperty("user.dir")
 
-
-        val args = listOf("build").map { Arg(it) }
+        val list = File( "testForTests" + File.separator + "second" + File.separator + "first" + File.separator + "first").absolutePath
+        val args = listOf(list).map { Arg(it) }
         val cmdCD = CommandFactoryImpl().createCommand(CommandType.CD, args)
         cmdCD.execute(VariableContextImpl())
 
-
-        val expected = File(startedPath).absolutePath + "\\build"
+        val expected = File(File(startedPath).absolutePath + File.separator + "testForTests" + File.separator +
+                "second" + File.separator + "first" + File.separator + "first").absolutePath
 
         Assertions.assertEquals(expected, SystemStateSingletonImpl.instance.getPath())
     }
 
+    //  main->testForTests->second->first->first->main
+    @Test
+    fun testExecuteCDValidAFewDots() {
+        var startedPath = System.getProperty("user.dir")
+
+        val list = File( "testForTests" + File.separator + "second" + File.separator + "first" + File.separator + "first").absolutePath
+        val args = listOf(list).map { Arg(it) }
+        val cmdCD = CommandFactoryImpl().createCommand(CommandType.CD, args)
+        cmdCD.execute(VariableContextImpl())
+
+        val argsSecond = listOf("../../../..").map { Arg(it) }
+        val cmdCDSecond = CommandFactoryImpl().createCommand(CommandType.CD, argsSecond)
+        cmdCDSecond.execute(VariableContextImpl())
+
+        val expected = File(startedPath).absolutePath
+
+        Assertions.assertEquals(expected, SystemStateSingletonImpl.instance.getPath())
+    }
+
+    //  main->globalParent just cd, for example to C:
+    @Test
+    fun testExecuteCDValidToGlobalParent() {
+        var startedPath = System.getProperty("user.dir")
+
+        val list = File(startedPath).absolutePath.split(File.separator)[0]
+        val args = listOf(list).map { Arg(it) }
+        val cmdCD = CommandFactoryImpl().createCommand(CommandType.CD, args)
+        cmdCD.execute(VariableContextImpl())
+
+        val expected = File(File(startedPath).absolutePath.split(File.separator)[0] + File.separator).absolutePath
+
+        Assertions.assertEquals(expected, SystemStateSingletonImpl.instance.getPath())
+    }
+
+
+    //  main->globalParent with "cd.." upper of global parent
+    @Test
+    fun testExecuteCDCheckUpperOfGlobalParent() {
+        var startedPath = System.getProperty("user.dir")
+
+        val list = File(startedPath).absolutePath.split(File.separator)[0]
+        val args = listOf(list).map { Arg(it) }
+        val cmdCD = CommandFactoryImpl().createCommand(CommandType.CD, args)
+        cmdCD.execute(VariableContextImpl())
+
+        val argsSecond = listOf("..").map { Arg(it) }
+        val cmdCDSecond = CommandFactoryImpl().createCommand(CommandType.CD, argsSecond)
+        val actual = cmdCDSecond.execute(VariableContextImpl())
+
+        val expected = Optional.of("Something went wrong: curFile.parentFile must not be null)\n" +
+                "Process finished with exit code 128.") to 0
+
+        Assertions.assertEquals(expected, actual)
+    }
+
+    //  main->testForTests->read tt.txt
     @Test
     fun testExecuteCDValidNextTwoDirWithCheckCat() {
         val args = listOf("testForTests").map { Arg(it) }
--- src/test/kotlin/com/architecturecourse/commandlineinterpreter/LsTests.kt
+++ src/test/kotlin/com/architecturecourse/commandlineinterpreter/LsTests.kt
@@ -10,8 +10,21 @@ import java.io.File
 
 class LsTests {
 
+//    TODO: дирректорию с папками для провреки уровней перехода. Тесты на переход сделать. С указанием конкретного файла
+//
     @Test
     fun testExecuteLsValidSimpleVariantOfLsCommand() {
+        val args = listOf<Arg>()
+        val cmdLS = CommandFactoryImpl().createCommand(CommandType.LS, args)
+        val actual = cmdLS.execute(VariableContextImpl())
+        var startedPath = System.getProperty("user.dir")
+        var dirFiles = actual.first.get().split("\n")
+
+        File(startedPath).listFiles().forEach {Assertions.assertTrue(dirFiles.contains(it.name))}
+    }
+
+    @Test
+    fun testExecuteLsValidSimpleVariantOfLsCommandOneTestDir() {
         val args = listOf<Arg>()
         val cmdLS = CommandFactoryImpl().createCommand(CommandType.LS, args)
         val actual = cmdLS.execute(VariableContextImpl())
